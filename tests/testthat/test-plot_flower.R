e70fa244c290ba51ee7d1a704a960167c1d703e8
Matt Jones <gitcode@magisa.org>
2019-12-06 04:30:18 -0900

    Fix test warnings.

--- tests/testthat/test-plot_flower.R
+++ tests/testthat/test-plot_flower.R
@@ -6,20 +6,18 @@ test_that("multiplication works", {
 # [8] "weight"          "name_supra"      "name_flower"     "pos"             "pos_end"         "pos_supra"       "plot_NA"
 
 test_that("flower plot displays", {
+    library(flowers)
+    library(dplyr)
     df <- data.frame(order = c(1, 4, 3, 2),
                         score = c(90, 80, 70, 60),
                         weight = c(1, 1, 1, 1),
                         goal = c("F", "A", "I", "R"),
                         name_flower = c("Findable", "Accessible", "Interoperable", "Reusable"),
-                        name_supra = c(NA, NA, "Meta", "Meta")
-                        )
+                        name_supra = c(NA, NA, "Meta", "Meta"),
+                        stringsAsFactors = FALSE) %>% arrange(order)
     expect_equal(ncol(df), 6)
     plot_flower(df, title = "FAIR Metrics")
-    df_sorted <- df %>% arrange(name_flower)
-    plot_flower(df_sorted, title = "FAIR Metrics")
-    df_sorted2 <- df %>% arrange(order)
-    plot_flower(df_sorted2, title = "FAIR Metrics")
 
-    ohi2 <- read.csv("ohi.csv", stringsAsFactors = FALSE)
-    plot_flower(ohi2, "OHI Example")
+    data(ohi)
+    plot_flower(ohi, "OHI Example")
 })
